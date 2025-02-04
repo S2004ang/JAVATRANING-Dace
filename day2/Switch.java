f44d064a05ac897d535aaf86a39d09998510ec3d
madhupavani <madhupavani908@gmail.com>
2025-02-04 15:45:04 +0530

    placement traning

---
+++ day2/Switch.java
@@ -0,0 +1,17 @@
+public class Switch {
+    public static void main(String[] args) {
+        int a=1;
+        switch(a){
+            case 1: a++;
+            break;
+            case 2: ++a;
+            break;
+            case 3: --a;
+            break;
+            default : a--;
+        }
+        System.out.println(a);
+
+    }
+    
+}
---
+++ day2/User_input.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class User_input {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // integer
+        int a= sc.nextInt();
+        System.out.println("Enter the integer");
+        //float
+        float f=sc.nextFloat();
+        System.out.println("Enter the float");
+        System.out.println(a);
+        System.out.println(f);
+        System.out.println(s);
+
+    }
+    
+}
